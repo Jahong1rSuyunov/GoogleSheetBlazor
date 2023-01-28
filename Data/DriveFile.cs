0d7274e90d11e7198c71d7099f64d3117184f775
Jahongir <jahongirsuyunov836@gmail.com>
2023-01-28 17:46:08 +0500

    Add project files.

---
+++ Data/DriveFile.cs
@@ -0,0 +1,10 @@
+﻿namespace GoogleSheetBlazor.Data
+{
+    public class DriveFile
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string MimeType { get; set; }
+        public string Kind { get; set; }
+    }
+}
